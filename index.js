d33a7ad8b955b20e45551e6640bd1f2fdfa8b9bf
Yeison Rojas <yeisonjr98@gmail.com>
2024-03-03 22:00:08 -0400

    add class

--- index.js
+++ index.js
@@ -1,285 +1,320 @@
-// TREE DESICION --------------------------------------------------
-class Node {
-  constructor(value, children = []) {
+class Career {
+  
+  constructor(name, value=0){
+    this.name = name;
     this.value = value;
-    this.children = children;
   }
+
+  increase(){
+    this.value += 1;
+  }
+
+  getName(){
+    return this.name;
+  }
+
+  getValue(){
+    return this.value;
+  }
+
 }
 
-class DecisionTree {
-  constructor(root) {
-    this.root = root;
+class Question {
+  
+  constructor(question,answer1,answer2,careers_true=null, careers_false=null){
+    this.question = question;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.careers_true = careers_true;
+    this.careers_false = careers_false;
   }
 
-  insertNode(value, parentValue) {
-
-    //console.log('VALORRR: ', value)
-    //console.log('VALORRRPARENTT: ', parentValue)
-
-    const newNode = new Node(value);
-    //console.log('NUEVO: ', newNode)
-
-    const parentNode = this.findNode(parentValue);
-    
-
-    parentNode.children.push(newNode);
-
-    //console.log('PADRE: ', parentNode)
-    
+  getQuestion(){
+    return this.question;
   }
 
-  findNode(value) {
-    let currentNode = this.root;
-    while (currentNode.value !== value) {
-      //console.log('ANALIZA: ',currentNode.value)
-      currentNode = currentNode.children.find((child) => child.value===value );
+  getAnswer1(){
+    return this.answer1;
+  }
+
+  getAnswer2(){
+    return this.answer2;
+  }
+
+  getCareersTrue(){
+    return this.careers_true;
+  }
+
+  getCareersFalse(){
+    return this.careers_false;
+  }
+
+}
+
+class HandlerDOM {
+  
+  constructor(careers, questions){
+
+    this.careers = careers;
+    this.questions = questions;
+    this.i = 0;
+
+  }
+
+  getCareers(){
+    return this.careers;
+  }
+
+  getQuiestions(){
+    return this.questions;
+  }
+
+  initTest(){
+    
+    const container = document.querySelector('.container');
+    let question = this.questions[this.i];
+    console.log(question);
+
+    const div = document.createElement('div');
+    div.classList.add('content');
+    div.innerHTML = this.createContainer(this.questions[this.i].question, this.questions[this.i].answer1, this.questions[this.i].answer2);
+    container.appendChild(div);
+  }
+
+  resetCareers(){
+    this.careers = [
+      new Career('Informatica'),
+      new Career('Arquitectura'),
+      new Career('Psicologia'),
+      new Career('Pedagogia'),
+      new Career('Derecho')
+    ];
+  }
+
+  createContainer(quest, res1, res2){
+
+    const div =
+    `
+      <h5 class="error"></h5>
+      <h3 class="quest">${quest}</h3>
+      <div class="options">
+        <div class="option">
+          <input type="radio" name="option" id="option1"><p id="answer-1">${res1}</p>
+        </div>
+        <div class="option">
+          <input type="radio" name="option" id="option2"><p id="answer-2">${res2}</p>
+        </div>
+      </div>
+    `;
+    
+    return div;
+  
+  }
+
+  // NEXT QUESTION
+  nextQuestion(){
+    
+    const container = document.querySelector('.container');
+    const content = document.querySelector('.content');
+    const radios = document.getElementsByName('option');
+    let question = this.questions[this.i];
+
+    // VERIFY SELECTION
+    if(!radios[0].checked && !radios[1].checked){
+      const error = document.querySelector('.error');
+      error.innerText = 'Debe seleccionar una opcion';
+      return;
     }
-    //console.log('SALIO: ', currentNode);
-    return currentNode;
-  }
 
-  evaluateTree(input) {
+    // PRINT OPTION SELECTED
+    if (radios[0].checked) {
 
-    // elimina espacios
-    input = input.trim();
-    
-    let currentNode = this.root;
+      if(question.careers_true) question.careers_true.forEach(item=>{
+        console.log(this.careers[item].getName());
+        this.careers[item].increase();
+      });
 
-    // console.log(typeof input)
-    // console.log(typeof input)
+    }
 
-    // console.log(input)
-    // console.log(currentNode.children[0].value)
-    // console.log(currentNode.children[0].value===input)
-    
-    //console.log(currentNode)
-    //console.log(currentNode.children.length)
-    const child = currentNode.children.find((child) => child.value==input );
-    //console.log('child:', child)
-    
-    if (child) {
+    if (radios[1].checked){
+      if(question.careers_false) question.careers_false.forEach(item=>{
+        console.log(this.careers[item].getName());
+        this.careers[item].increase();
+      });
+
+    }
+
+    console.log(this.careers);
+
+    // INCREASE COUNTER
+    this.i += 1;
+
+    // IF THERE ARE MORE QUESTIONS
+    if (this.i<this.questions.length) {
+
+      question = this.questions[this.i];
+      console.log(question);
       
-      currentNode = child;
+      const div = document.createElement('div');
+      div.innerHTML = this.createContainer(this.questions[this.i].getQuestion(), this.questions[this.i].getAnswer1(), this.questions[this.i].getAnswer2());
+      div.classList.add('content');
+      container.removeChild(content);
+      container.appendChild(div);
     
     } else {
-      console.log("No hay una decisión para la entrada: " + input)
-      return null;
+
+      // FINISH TEST
+      const app = document.querySelector('.app');
+      const btnSend = document.querySelector('button');
+      const div = document.createElement('div');
+      const btnReset = document.createElement('button');
+
+      console.log("Resultado final:");
+      console.log(this.careers);
+
+      // las ordena de forma descendente
+      this.careers.sort(function(a, b){
+        return b.getValue() - a.getValue();
+      });
+
+      console.log(this.careers);
+      
+      div.innerHTML = 
+      `<h3>Felicidades ya terminó!</h3>
+      `;
+      btnReset.innerText = 'Volver'
+
+      div.classList.add('content');
+      container.removeChild(content);
+      app.removeChild(btnSend);
+      container.appendChild(div);
+      app.appendChild(btnReset);
+
+      btnReset.addEventListener("click", ()=> {
+        this.testReset();
+      });
+
     }
 
-    return currentNode;
   }
+
+  // RESET TEST
+  testReset(){
+
+    const app = document.querySelector('.app');
+    const container = document.querySelector('.container');
+    const content = document.querySelector('.content');
+    const btnReset = document.querySelector('button');
+
+    const div = document.createElement('div');
+    const btnSend = document.createElement('button');
+
+    div.classList.add('content');
+    btnSend.innerText = 'Responder';
+
+    btnSend.addEventListener("click",()=> {
+      this.nextQuestion();
+    });
+
+    this.i=0;
+
+    this.careers = [
+      new Career('Informatica'),
+      new Career('Arquitectura'),
+      new Career('Psicologia'),
+      new Career('Pedagogia'),
+      new Career('Derecho')
+    ];
+
+    app.removeChild(btnReset);
+    container.removeChild(content);
+
+    // primera pregunta
+    let question = this.questions[this.i];
+    console.log(question);
+
+    div.innerHTML = this.createContainer(questions[this.i].getQuestion(), questions[this.i].getAnswer1(), questions[this.i].getAnswer2());
+    // inserta pregunta
+    container.appendChild(div);
+    // inserta button
+    app.appendChild(btnSend);
+
+  }
+
 }
 
-// Ejemplo de uso
-const tree = new DecisionTree(new Node("¿Es un animal peludo?"));
 
-tree.insertNode("si", "¿Es un animal peludo?");
-tree.insertNode("no", "¿Es un animal peludo?");
-tree.insertNode("Es un gato", "si");
-tree.insertNode("Es una gallina", "no");
-
-
-// FRONTEND ------------------------------------------------
+// CAREERS
+let careers = [
+  new Career('Informatica'),
+  new Career('Arquitectura'),
+  new Career('Psicologia'),
+  new Career('Pedagogia'),
+  new Career('Derecho')
+];
 
 // QUESTIONS
 const questions = [
-  {
-    question: '¿Te gustan salir con tus amigos de fiesta?',
-    answer1: 'Sí, me gusta salir de fiesta.',
-    answer2: 'No, prefiero una reunión tranquila.'
-  },
-  {
-    question: '¿En qué área te consideras mejor?',
-    answer1: 'Matemáticas.',
-    answer2: 'Inglés.'
-  },
-  {
-    question: '¿Eres bueno con la tecnología?',
-    answer1: 'Sí, soy bueno con la tecnología.',
-    answer2: 'No me llama la atención.'
-  },
-  {
-    question: '¿Te gustan las manualidades?',
-    answer1: 'Sí, me gustan las manualidades.',
-    answer2: 'No me gustan las manualidades.'
-  },
-  {
-    question: '¿Te gustan contruir cosas?',
-    answer1: 'Sí, me gusta usar mi creatividad.',
-    answer2: 'No me llama la atención.'
-  }
-]
-
-// {
-//   question: '¿Las personas suelen contarte sus problemas?',
-//   answer1: 'Sí, soy bueno escuchando.',
-//   answer2: 'No, tengo pocos amigos.'
-// },
-
-//const preguntas = ['Que dia es hoy?','Que hora es?'];
-
-// const respuestas = [
-//   {
-//     res1: 'res1',
-//     res2: 'res2'
-//   },
-//   {
-//     res1: 'res3',
-//     res2: 'res4'
-//   },
-// ];
-
-let i = 0
-
-// SELECTION
-const app = document.querySelector('.app');
-
-const container = document.querySelector('.container');
-
-const content = document.querySelector('.content');
-
-const quest = document.querySelector('.quest');
-
-const options = document.querySelector('.options');
+  new Question(
+    '¿Te gustan salir con tus amigos de fiesta?',
+    'Sí, me gusta salir de fiesta.',
+    'No, prefiero una reunión tranquila.',
+    [1,2,3,4], // arq, psico, pedago, derecho
+    [0] // infor
+  ),
+  new Question(
+    '¿En qué área te consideras mejor?',
+    'Matemáticas.',
+    'Inglés.',
+    [0,1], // infor, arq
+    [2,3,4] // psico, pedago, derecho
+  ),
+  new Question(
+    '¿Eres bueno con la tecnología?',
+    'Sí, soy bueno con la tecnología.',
+    'No me llama la atención.',
+    [0], // infor
+    [1,2,3,4] // arq, psico, pedago, derecho
+  ),
+  new Question(
+    '¿Te gustan las manualidades?',
+    'Sí, me gustan las manualidades.',
+    'No me gustan las manualidades.',
+    [1,3], // arq, pedago
+    [0,2,4] // infor, psico, derecho
+  ),
+  new Question(
+    '¿Te gustan contruir cosas?',
+    'Sí, me gusta usar mi creatividad.',
+    'No me llama la atención.',
+    [0,1,3], // infor, arq, pedago
+    [2,4] // psico, derecho
+  ),
+  new Question(
+    '¿Te gustan mantener todo en orden?',
+    'Sí, soy una persona muy ordenada.',
+    'Me es indiferente.',
+    [4], // derecho
+  ),
+  new Question(
+    '¿Las personas suelen contarte sus problemas?',
+    'Sí, mis amigos siempre me cuentas sus problemas.',
+    'No me pasa muy seguido.',
+    [2], // pedago
+  ),
+  new Question(
+    '¿Eres bueno explicando cosas?',
+    'Sí, me considero bueno explicando temas.',
+    'No soy muy bueno hablando.',
+    [3], // pedago
+  ),
+];
 
 const btnSend = document.querySelector('button');
 
-
-const createQuest = (quest, res1, res2) => {
-
-  const div =
-  `
-    <h5 class="error"></h5>
-    <h3 class="quest">${quest}</h3>
-    <div class="options">
-      <div class="option">
-        <input type="radio" name="option" id="option1"><p id="answer-1">${res1}</p>
-      </div>
-      <div class="option">
-        <input type="radio" name="option" id="option2"><p id="answer-2">${res2}</p>
-      </div>
-    </div>
-  `;
-  
-  return div;
-
-}
-
-// NEXT QUESTION
-const nextQuestion = () => {
-  
-  const content = document.querySelector('.content');
-  const radios = document.getElementsByName('option');
-  const option1 = document.querySelector('#answer-1');
-  const option2 = document.querySelector('#answer-2');
-
-  // VERIFY SELECTION
-  if(!radios[0].checked && !radios[1].checked){
-    const error = document.querySelector('.error');
-    error.innerText = 'Debe seleccionar una opcion';
-    return;
-  }
-
-  // PRINT OPTION SELECTED
-  if (radios[0].checked) console.log(option1.innerText);
-  if (radios[1].checked) console.log(option2.innerText);
-
-  // console.log(option1.innerText,radios[0].checked);
-  // console.log(option2.innerText,radios[1].checked);
-
-  // AMOUNT COUNTER
-  i += 1;
-
-  // IF THERE ARE MORE QUESTIONS
-  if (i<questions.length) {
-    
-    const div = document.createElement('div');
-    div.innerHTML = createQuest(questions[i].question, questions[i].answer1, questions[i].answer2);
-    div.classList.add('content');
-    container.removeChild(content);
-    container.appendChild(div);
-  
-  } else {
-
-    // FINISH TEST
-    const btnSend = document.querySelector('button');
-    const div = document.createElement('div');
-    const btnResert = document.createElement('button');
-    
-    div.innerHTML = '<h3>Felicidades ya termino!</h3>';
-    btnResert.innerText = 'Volver'
-
-    div.classList.add('content');
-    container.removeChild(content);
-    app.removeChild(btnSend);
-    container.appendChild(div);
-    app.appendChild(btnResert);
-
-    btnResert.addEventListener("click", ()=> {
-      testReset();
-    });
-  }
-
-}
-
-// RESERT TEST
-const testReset = () => {
-
-  const content = document.querySelector('.content');
-  const btnResert = document.querySelector('button');
-
-  const div = document.createElement('div');
-  const btnSend = document.createElement('button');
-
-  div.classList.add('content');
-  btnSend.innerText = 'Responder';
-
-  btnSend.addEventListener("click",()=> {
-    nextQuestion();
-  });
-
-  i=0;
-
-  app.removeChild(btnResert);
-  container.removeChild(content);
-
-  // primera pregunta
-  div.innerHTML = createQuest(questions[i].question, questions[i].answer1, questions[i].answer2);
-  // inserta pregunta
-  container.appendChild(div);
-  // inserta button
-  app.appendChild(btnSend);
-
-}
-
 btnSend.addEventListener("click",()=> {
-  nextQuestion();
+  test.nextQuestion();
 });
 
-// crea primera pregunta --------------------------------------------------
-const div = document.createElement('div');
-div.classList.add('content');
-div.innerHTML = createQuest(questions[i].question, questions[i].answer1, questions[i].answer2);
-container.appendChild(div);
-
-//console.log(tree.root.value);
-
-// let res = process.openStdin();
-
-// res.addListener("data", data => {
-
-//   //console.log("respuesta: ", data.toString()); 
-//   res = tree.evaluateTree(data.toString());
-//   console.log(res.children[0].value);
-  
-
-// });
-
-// let decision = '';
-
-// while(decision!==null){
-
-//   decision = tree.evaluateTree("no");
-//   console.log(decision); // Salida: "Perro"
-
-// }
+const test = new HandlerDOM(careers,questions);
+test.initTest();
